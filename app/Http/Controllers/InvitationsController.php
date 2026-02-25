5b549fe8c14a176dfe3cfbad1c5d0fc16053a4c0
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-02-25 16:02:25 +0100

    feat: add InvitationController and FormRequest validation

--- app/Http/Controllers/InvitationsController.php
+++ app/Http/Controllers/InvitationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Invitations;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreInvitationsRequest;
 
 class InvitationsController extends Controller
 {
@@ -20,15 +21,20 @@ class InvitationsController extends Controller
      */
     public function create()
     {
-        //
+        return view('invitations.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreInvitationsRequest $request)
     {
-        //
+        Invitations::create([
+            'email' => $request->email,
+            
+            'colocation_id' => $request->colocation_id,
+        ]);
+        return redirect()->route('invitation.index')->with('success','create invitation avec success');
     }
 
     /**
@@ -42,17 +48,23 @@ class InvitationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Invitations $invitations)
+    public function edit(string $id)
     {
-        //
+        $invitations = Invitations::findOrFail($id);
+        return view('invitations.edit',compact('invitations'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Invitations $invitations)
+    public function update(StoreInvitationsRequest $request, Invitations $invitations)
     {
-        //
+        Invitations::updated(  [
+             'email' => $request->email,
+            
+            'colocation_id' => $request->colocation_id,
+        ]);
+        return redirect()->route('colocation.index')->with('success','invitations modifier avec success');
     }
 
     /**
