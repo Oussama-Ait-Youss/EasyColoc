e37fc06807fdc30278fe573ea88524dc54e2c16c
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-02-25 15:56:59 +0100

    feat: add paymentController and FormRequest validation

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Payments;
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePaymentRequest;
+
+
 
 class PaymentsController extends Controller
 {
@@ -12,7 +15,8 @@ class PaymentsController extends Controller
      */
     public function index()
     {
-        //
+        $payments = Payments::all();
+        return view('payments.index',compact('payments'));
     }
 
     /**
@@ -20,15 +24,22 @@ class PaymentsController extends Controller
      */
     public function create()
     {
-        //
+        return view('payments.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePaymentRequest $request)
     {
-        //
+        Payments::created([
+            'amount' => $request->amount,
+
+            'from_user_id' => $request->from_user_id,
+
+            'colocation_id' => $request->colocation_id,
+        ]);
+        return redirect()->route('payments.index')->with('success','payment avec success');
     }
 
     /**
@@ -42,17 +53,26 @@ class PaymentsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Payments $payments)
-    {
-        //
+    public function edit(string $id)
+    {   
+        $payments = Payments::findOrFail($id);
+        return view('payment.edit',compact('payments'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Payments $payments)
+    public function update(StorePaymentRequest $request, Payments $payments)
+
     {
-        //
+        Payments::updated([
+            'amount' => $request->amount,
+
+            'from_user_id' => $request->from_user_is,
+
+            'colocation_id' => $request->colocation_id,
+        ]);
+        return redirect()->route('payments.index')->with('success','payment modifier avec success');
     }
 
     /**
