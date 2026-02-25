a72ef26ee68f843fbc2ed845a62cd32de2e48e5d
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-02-25 15:54:32 +0100

    feat: add CategoriesController and FormRequest validation

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Categories;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoriesRequest;
 
 class CategoriesController extends Controller
 {
@@ -20,15 +21,20 @@ class CategoriesController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoriesRequest $request)
     {
-        //
+        Categories::created([
+                'name' => $request->name,
+            
+                'colocation_id' => $request->colocation_id,
+        ]);
+        return redirect()->route('categories.index')->with('success','create category avec success');
     }
 
     /**
@@ -42,17 +48,24 @@ class CategoriesController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Categories $categories)
+    public function edit(string $id)
     {
-        //
+        $categories = Categories::findOrFail($id);
+        return view('categories.edit',compact('categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Categories $categories)
+    public function update(StoreCategoriesRequest $request, Categories $categories)
     {
-        //
+        Categories::updated([
+
+            'name' => $request->name,
+            'colocation_id' => $request->colocation_id,
+            
+        ]);
+        return redirect()->route('categories.index')->with('success','category modifier avec success');
     }
 
     /**
